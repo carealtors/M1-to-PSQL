4c0a4ac406d26edf51337b003b7f8e5edb44b1b1
David Conroy <dconroy@gmail.com>
2025-02-18 16:59:06 -0600

    adding external interface

--- api/app/models/__init__.py
+++ api/app/models/__init__.py
@@ -4,3 +4,4 @@ from .manual_eft import ManualEFT
 from .chargeback import Chargeback
 from .dues import DuesPayments
 from .ecp_view import DuesPaymentsSummary
+from .external_interface import ExternalInterface
